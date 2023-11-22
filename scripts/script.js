9d8ae989ed91beca35e80f4387c25e3cf078ca45
Maciej Rajtar <rajmac93@gmail.com>
2023-11-22 21:46:17 +0100

    done with nav

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,6 @@
 const toggleMenu = () => {
-  console.log("dupa");
+  const menu = document.querySelector(".menu--links");
+  const icon = document.querySelector(".hamburger--icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
 };
