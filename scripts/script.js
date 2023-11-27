ddf5dcb7ab1ec5572b7c6fb86bed3bf8cc950f7a
Maciej Rajtar <rajmac93@gmail.com>
2023-11-27 21:50:17 +0100

    added progres bar, some changes in styles

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,23 @@
+const pageProgressBar = document.querySelector(".progress--bar");
+
+const showOnPx = 100;
+
 const toggleMenu = () => {
   const menu = document.querySelector(".menu--links");
   const icon = document.querySelector(".hamburger--icon");
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 };
+
+const scrollContainer = () => {
+  return document.documentElement || document.body;
+};
+
+document.addEventListener("scroll", () => {
+  const scrolledPercentage =
+    (scrollContainer().scrollTop /
+      (scrollContainer().scrollHeight - scrollContainer().clientHeight)) *
+    100;
+
+  pageProgressBar.style.width = `${scrolledPercentage}%`;
+});
